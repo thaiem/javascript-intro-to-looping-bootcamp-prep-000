8bdd954cae1bf4276e58a3c29a8fe92df1429c87
Elaine Thai <ethai1827@gmail.com>
2019-05-14 22:20:03 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -21,7 +21,7 @@ function doWhileLoop(integer) {
   function incrementVariable() {
   integer = integer + 1;
   return integer;
-}
+  }
   do {
     console.log("I run once regardless.");
   } while (incrementVariable() < integer);
